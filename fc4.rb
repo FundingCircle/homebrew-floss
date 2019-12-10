3df6920beed18ad0fcf05d4b3399fcaf96786b81
Avi Flax <avi.flax@fundingcircle.com>
2019-12-10 10:34:23 -0500

    Update FC4 to 2019.12.10-3d660b6

--- fc4.rb
+++ fc4.rb
@@ -2,9 +2,9 @@
 class Fc4 < Formula
   desc "Tool for C4 software architecture diagrams"
   homepage "https://fundingcircle.github.io/fc4-framework/tool/"
-  version "2019.10.04-117ecb9"
-  url "https://github.com/FundingCircle/fc4-framework/releases/download/release_2019.10.04-117ecb9/fc4-tool-2019.10.04-117ecb9.tar.gz"
-  sha256 "eb54afe74d20b84f7425622f50ae1eafb451d0dbdacf441a1861184bc0b100cb"
+  version "2019.12.10-3d660b6"
+  url "https://github.com/FundingCircle/fc4-framework/releases/download/release_2019.12.10-3d660b6/fc4-tool-2019.12.10-3d660b6.tar.gz"
+  sha256 "894138bd50f67da8b8612c59b7d28827890a718cab8dbbba8f70be889df05bb1"
 
   bottle :unneeded
   depends_on :java => "1.8+"
